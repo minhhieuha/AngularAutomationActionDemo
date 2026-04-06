d83da15e80fd8280ae3a5dc35d0090c048ef7891
James Ha <hieu.ha@productaddon.com>
2026-04-06 16:12:29 +0700

    Test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,11 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:4202/');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('james.ha');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('test');
+test('recorded login flow', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByRole('button', { name: 'Sign Out' }).click();
 });
